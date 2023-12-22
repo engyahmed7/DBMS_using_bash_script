53bf1fdbf3d3045983cc2884a458b597ffafc7cd
Engy Ahmed Abdelaziz <engya306@gmail.com>
2023-12-22 20:18:18 +0200

    Refactor insertTable.sh script to improve readability and error handling

--- TB_dir/insertTable.sh
+++ TB_dir/insertTable.sh
@@ -7,46 +7,48 @@ table_dir="$HOME/$script_dir/db_dir/$1"
 table_file="$table_dir/$tbname"
 table_tp="$table_file"
 
-if [[ -z "$tbname" || "$tbname" =~ [/.:\\-] ]]; then
+while [[ -z "$tbname" || "$tbname" =~ [/.:\\-] ]]; do
     echo "Error: Table name cannot be empty or have special characters. Please enter a valid name."
-elif [[ ! -f "$table_file" ]]; then
+    read -p "Enter Table Name: " tbname
+done
+
+while [[ ! -f "$table_file" ]]; do
     echo "Error: Table $tbname does not exist."
-else
-    columns=($(cut -d: -f1 "$table_file"))
-    datatypes=($(cut -d: -f2 "$table_tp"))
-    datatypespk=($(cut -d: -f2,3 "$table_tp"))
-    record=""
-    primary_key=""
+    read -p "Enter Table Name: " tbname
+done
 
-    touch "$table_dir/records_${tbname}.txt"
+columns=($(cut -d: -f1 "$table_file"))
+datatypes=($(cut -d: -f2 "$table_tp"))
+datatypespk=($(cut -d: -f2,3 "$table_tp"))
+record=""
+primary_key=""
 
-    for ((i = 0; i < ${#columns[@]}; i++)); do
-        col="${columns[i]}"
-        dtype="${datatypes[i]}"
+touch "$table_dir/records_${tbname}.txt"
 
-        while true; do
-            read -p "Enter $col ($dtype): " value
+for ((i = 0; i < ${#columns[@]}; i++)); do
+    col="${columns[i]}"
+    dtype="${datatypes[i]}"
 
-            if [[ -z "$value" ]]; then
-                echo "Error: $col cannot be empty."
-            elif [[ "$dtype" == "int" && ! "$value" =~ ^[0-9]+$ ]]; then
-                echo "Error: $col must be an integer."
-            else
-                record+="$value:"
-                if [[ "${datatypespk[i]}" == *":pk" ]]; then
-                    primary_key="$value"
-		    
-                fi
-                break
+    while true; do
+        read -p "Enter $col ($dtype): " value
+
+        if [[ -z "$value" ]]; then
+            echo "Error: $col cannot be empty."
+        elif [[ "$dtype" == "int" && ! "$value" =~ ^[0-9]+$ ]]; then
+            echo "Error: $col must be an integer."
+        else
+            record+="$value:"
+            if [[ "${datatypespk[i]}" == *":pk" ]]; then
+                primary_key="$value"
             fi
-        done
+            break
+        fi
     done
+done
 
-    if grep "^$primary_key:" "$table_dir/records_${tbname}.txt" | cut -d: -f1 | grep -q "^$primary_key$"; then
-        echo "Error: Primary key must be unique. Record with $primary_key already exists."
-    else
-        echo "$record" >> "$table_dir/records_${tbname}.txt"
-        echo "Record inserted successfully into $tbname table."
-    fi
-
+if grep "^$primary_key:" "$table_dir/records_${tbname}.txt" | cut -d: -f1 | grep -q "^$primary_key$"; then
+    echo "Error: Primary key must be unique. Record with $primary_key already exists."
+else
+    echo "$record" >> "$table_dir/records_${tbname}.txt"
+    echo "Record inserted successfully into $tbname table."
 fi
